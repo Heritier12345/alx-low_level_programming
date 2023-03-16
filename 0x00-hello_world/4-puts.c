1a3c2f406c32abcbd7f60c482c3365434e952d9e
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-16 14:14:20 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Programming is like building a multilingual puzzle
+ * main -  Programming is like building a multilingual puzzle
  * Return 0 (Success)
  */
 int main(void)
