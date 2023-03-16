93890dc26ebc75e98d2dfc20e66666e16d879d4e
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-16 15:04:59 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - For now i can undertstand
+ * Return: 0 (Coming)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" -Dora Kor par, 2015-10-19\n", stdout);
+	return (1);
+}
