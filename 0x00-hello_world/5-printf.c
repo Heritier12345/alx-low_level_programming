2ead7142c27f0905d2aae0154b897d8956b0b3dd
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-16 14:22:57 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Diving through programs
+ * Return: 0 (Brave)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
