21d03d303f67bdc0c44af40739e6c68c87017518
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-21 06:48:29 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  * Description: 'print alphabet in lowercase'
  * Return: Always 0
  */
-int main(void)
+int _putchar(void)
 {
 	putchar("_putchar");
 	putchar('\n');
