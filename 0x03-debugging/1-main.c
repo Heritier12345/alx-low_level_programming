1544c1e61bd5b074578ece83fcf50e3758c64be9
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-23 13:38:42 -0700

    Like, comment, subcribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Causes an infinite lopp
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming : (\n");
+
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/*    puchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
