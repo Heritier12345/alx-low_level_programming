41550f6f2e9e5b50f1de17b80fc4fcfcd2930339
Heritier12345 <heritierkimenyi@gmail.com>
2023-03-30 01:20:52 -0700

    Function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,6 @@ char *cap_string(char *str)
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
-				str[index - 1] == ' ' ||
 				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
