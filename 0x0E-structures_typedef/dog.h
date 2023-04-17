b76292e25932b032c2ffd6baa5285657af432c6a
Heritier12345 <heritierkimenyi@gmail.com>
2023-04-17 03:11:34 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
