668efdfdb6b93ef9922ed6aa7a1c8a661d336b3f
Heritier12345 <heritierkimenyi@gmail.com>
2023-04-30 11:54:39 -0700

    Function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,9 +3,9 @@
 /**
  * find_listint_lopp - Finds the loop in a linked list
  * @head: linked list to search for
+ *
  * Return: address of the node where the loop starts, or NULL
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
